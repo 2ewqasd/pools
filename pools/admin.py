e4f511903c7738e640930e2486180a815e2827ba
Vlad Radchenko <specforworkwithpeop@gmail.com>
2021-01-18 20:55:18 +0300

    Register models in admin

--- pools/admin.py
+++ pools/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Pool
+from .models import Question
+from .models import SuggestedAnswer
+from .models import Answer
 
-# Register your models here.
+admin.site.register(Pool)
+admin.site.register(Question)
+admin.site.register(SuggestedAnswer)
+admin.site.register(Answer)

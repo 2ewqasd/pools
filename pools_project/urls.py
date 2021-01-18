06965c4a83c7219a0bdd6021157b362075bd731d
Vlad Radchenko <specforworkwithpeop@gmail.com>
2021-01-18 23:26:43 +0300

    Add OpenApi and Swagger

--- pools_project/urls.py
+++ pools_project/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('pools.urls'))
+    path('api/v1/', include('pools.urls')),
+    path('openapi', get_schema_view(
+        title="Example",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
